a7e0445ef8ae50b6abb5222566a31f3c500634a2
gubshayad <shayadryhan@gmail.com>
2019-07-01 19:01:33 +0600

    Shortcode portfolio create

--- wp-content/themes/Moderna/inc/shortcode.php
+++ wp-content/themes/Moderna/inc/shortcode.php
@@ -71,12 +71,13 @@
 
     function post_list_shortcode($atts, $content = null){
         extract(shortcode_atts( array(
-    
+                'type'=>'post',
+                'count'=>'count',
         ), $atts )); 
 
         $q= new Wp_Query(
             array(
-                'post_per_page'=>'5',
+                'posts_per_page'=>$count,
                 'post_type'=>$type,
             ));
 
@@ -93,5 +94,54 @@
     }
 
     add_shortcode( 'post', 'post_list_shortcode');
-    
+
+    function test_portfolio_shortcode($atts, $content = null){
+
+        extract(shortcode_atts( array(
+           'count'=>'10',
+           'type'=>'portfolio'
+    ), $atts )); 
+
+    $q= new Wp_Query(
+        array(
+            'posts_per_page'=>$count,
+            'post_type'=>$type,
+        ));
+     $list='<div class="row">
+                <section id="projects">
+                    <ul id="thumbs" class="portfolio">';
+
+             while($q->have_posts()): $q->the_post();
+
+              $portfolio_large= wp_get_attachment_image_src( get_post_thumbnail_id ($post->ID), 'large'); 
+              $portfolio_small = wp_get_attachment_image_src( get_post_thumbnail_id ($post->ID), 'portfolio_thumb'); 
+
+                    $list.=' 
+                        
+                        <li class="col-lg-3 design" data-id="id-0" data-type="web">
+                        <div class="item-thumbs">
+                            <!-- Fancybox - Gallery Enabled - Title - Full Image -->
+
+                            <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Work 1"
+                                href="'.$portfolio_large[0].'">
+
+                                <span class="overlay-img"></span>
+                                <span class="overlay-img-thumb font-icon-plus"></span>
+                            </a>
+                            <!-- Thumb Image and Description -->
+                            <img src="'.$portfolio_small[0].'" alt="'.get_the_title().'"/>
+                         
+
+                        </div>
+                    </li>';
+                   
+            endwhile;
+
+     $list.="</ul></section></div>";   
+     wp_reset_query();
+     return $list;
+
+    }
+
+    add_shortcode('portfolio_list', 'test_portfolio_shortcode');
 ?>
