1d53ab654db70141b08136cd48ef1c3c74780e4b
gubshayad <gubshayad@gmail.com>
2019-07-07 10:01:06 +0600

    add top bar

--- wp-content/themes/Moderna/header.php
+++ wp-content/themes/Moderna/header.php
@@ -21,7 +21,34 @@
 <body>
 	<div id="wrapper">
 		<!-- start header -->
-		<header>
+		<header id="header">
+			<div class="top-bar">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-6 col-xs-4">
+							<div class="top-number"><p><i class="fa fa-phone-square"></i>  +0123 456 70 90</p></div>
+						</div>
+						<div class="col-sm-6 col-xs-8">
+						<div class="social">
+								<ul class="social-share">
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-linkedin"></i></a></li> 
+									<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+									<li><a href="#"><i class="fa fa-skype"></i></a></li>
+								</ul>
+								<div class="search">
+									<form role="form">
+										<input type="text" class="search-form" autocomplete="off" placeholder="Search">
+										<i class="fa fa-search"></i>
+									</form>
+							</div>
+						</div>
+						</div>
+					</div>
+				</div><!--/.container-->
+			</div><!--/.top-bar-->
+
 			<div class="navbar navbar-default navbar-static-top">
 				<div class="container">
 					<div class="navbar-header">
